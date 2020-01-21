151d144f4ca2a1e1dad9ebe59f2d3e609c51740f
南志於 <au341998@gmail.com>
2020-01-22 01:33:22 +0900

    Kansatsuシーン スクリプト作成 KansatsuManager.CS(修正) SchaleKin.CS

--- Assets/Script/Kansatsu/KansatsuManager.cs
+++ Assets/Script/Kansatsu/KansatsuManager.cs
@@ -6,6 +6,63 @@ using TMPro;
 
 public class KansatsuManager : MonoBehaviour
 {
+
+    [Header("シャーレ用キンのプレファブ")]
+    public SchaleKin schaleKinPrefab;
+
+    [Header("キンの生成位置")]
+    public Transform[] shaleKinTrans;
+
+    [Header("キンの属性")]
+    public KIN_TYPE[] kinTypes;
+
+    [Header("キンの名前")]
+    public string[] kinNames;
+
+    [Header("キンの名前表示用")]
+    public TMP_Text[] txtKinNames;
+
+    [Header("キンのイメージ表示用")]
+    public Image[] imgKinImages;
+
+    [Header("キンの属性イメージ表示用")]
+    public Image[] imgKinTypes;
+
+    [Header("キンのタグ")]
+    public string[] kinTags;
+
+    [Header("一回あたりのキンの最小生成数")]
+    public int minNumber;
+
+    [Header("一回あたりのキンの最大生成数")]
+    public int maxNumber;
+
+    [Header("待機時間の最小値")]
+    public int minWaitTime;
+
+    [Header("待機時間の最大値")]
+    public int maxWaitTime;
+
+    [Header("待機時間")]
+    public int[] waitTimes;
+
+    [Header("待機時間のカウント用")]
+    public float[] timers;
+
+    [Header("観察スタートのフラグ")]
+    public bool isKansatsuStart;
+
+    [Header("上側の生成されたキンの総数")]
+    public int enemyCount;
+
+    [Header("下側の生成されたキンの総数")]
+    public int rivalCount;
+
+
+    //画面用UI
+    public TMP_Text txtTime;
+    public TMP_Text txtEnemyCount;
+    public TMP_Text txtRivalCount;
     public Button returnButton;
 
     
@@ -17,8 +74,130 @@ public class KansatsuManager : MonoBehaviour
         //UIヘッダー隠す
         UIManager.instance.SwitchDisplayCanvas(0);
 
-
+        //ボタンの登録
         returnButton.onClick.AddListener(() => StartCoroutine(SceneStateManager.instance.MoveScene(SCENE_TYPE.HOME)));
+
+        //キンの生成数の表示を更新
+        UpdateDisplayKinCount();
     }
 
+
+
+    /// <summary>
+    /// ポップアップで設定したキンの情報を受け取り、変数に登録する
+    /// </summary>
+    /// <param name="names"></param>
+    /// <param name="types"></param>
+    public void SetUpShaleKinInfo(string[] names, KIN_TYPE[] types, int maxValue)
+    {
+        for (int i = 0; i < names.Length; i++)
+        {
+            //キンの名前と属性登録
+            kinNames[i] = names[i];
+            kinTypes[i] = types[i];
+
+            //生成までにかかる時間を設定
+            waitTimes[i] = Random.Range(minWaitTime, maxWaitTime);
+
+            //キンの名前を画面に表示する
+            txtKinNames[i].text = names[i];
+            Debug.Log(names[i]);
+
+            //キンのイメージと属性のイメージを画面に表示
+            imgKinImages[i].sprite = Resources.Load<Sprite>("Image/" + kinNames[i]);
+            imgKinTypes[i].sprite = Resources.Load<Sprite>("Type/" + kinTypes[i]);
+
+            timers[i] = 0;
+        }
+
+        //一回あたりの生成数の最大値を受け取る
+        maxNumber = maxValue;
+
+        //設定が終わったので観察を開始する
+        isKansatsuStart = true;
+    }
+
+
+
+
+    void Update()
+    {
+        if (!isKansatsuStart)
+        {
+            //設定するまでは動かない
+            return;
+        }
+        else
+        {
+            //キンその1の生成までのカウントを行う
+            timers[0] += Time.deltaTime;
+
+            if (timers[0] >= waitTimes[0])
+            {
+                timers[0] = 0;
+
+                //シャーレキンの生成をする
+                CreateShaleKins(kinNames[0], kinTypes[0], kinTags[0], shaleKinTrans[0], true);
+                //次回の生成までの待機時間を設定
+                waitTimes[0] = Random.Range(minWaitTime, maxWaitTime);
+            }
+
+            //キンその２の生成までのカウントを行う
+            timers[1] += Time.deltaTime;
+            if (timers[1] >= waitTimes[1])
+            {
+                timers[1] = 0;
+                CreateShaleKins(kinNames[1], kinTypes[1], kinTags[1], shaleKinTrans[1], false);
+                waitTimes[1] = Random.Range(minWaitTime, maxWaitTime);
+            }
+        }
+    }
+
+
+
+    /// <summary>
+    /// シャーレキンを生成する
+    /// </summary>
+    public void CreateShaleKins(string name, KIN_TYPE type, string tag, Transform tran, bool isMoveDir)
+    {
+        //今回生成するキンの数をランダムに設定
+        int spawnCount = Random.Range(minNumber, maxNumber);
+
+
+        int value = 0;
+        for (int i = 0; i < spawnCount; i++)
+        {
+            //生成回数だけシャーレキンを生成
+            SchaleKin schaleKin = Instantiate(schaleKinPrefab, tran, false);
+            schaleKin.Init(name, type, tag, isMoveDir);
+            value++;
+        }
+
+        //生成したキンを合計する
+        if (isMoveDir)
+        {
+            enemyCount += value;
+        }
+        else
+        {
+            rivalCount += value;
+        }
+
+        //キンの生成数の表示を更新
+        UpdateDisplayKinCount();
+
+    }
+
+    /// <summary>
+    /// 生成されたキンのカウントを更新して表示
+    /// </summary>
+    private void UpdateDisplayKinCount()
+    {
+        txtEnemyCount.text = enemyCount.ToString();
+        txtRivalCount.text = rivalCount.ToString();
+
+    }
 }
+
+
+
---
+++ Assets/Script/Kansatsu/SchaleKin.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SchaleKin : MonoBehaviour
+{
+    [Header("自分のリジッドボディを入れる")]
+    public Rigidbody2D rb;
+
+    [Header("自分のtag")]
+    public string myTag;
+
+    [Header("相手のtag")]
+    public string enemyTag;
+
+    [Header("移動速度")]
+    public float speed;
+
+    [Header("キンのタイプ")]
+    public KIN_TYPE kinType;
+
+    [Header("キンの画像イメージ")]
+    public Image imgKinImage;
+
+    private bool isSetUp; //セット終了のフラグ
+    private Vector2 direction; //移動する方向
+
+    public enum State
+    {
+        //キンの状態用
+        STOP,
+        SET_UP,
+        MOVE,
+    }
+
+
+    [Header("キンの現在の状態")]
+    public State state = State.STOP; //初期値を設定しておく
+
+
+    /// <summary>
+    /// 初期設定を行う。このクラスとインスタンスと同時に呼び出してもらう
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="type"></param>
+    /// <param name="tag"></param>
+    /// <param name="isMoveDir"></param>
+    public void Init(string name, KIN_TYPE type, string tag, bool isMoveDir)
+    {
+        //イメージキンとキンタイプの表示設定
+        imgKinImage.sprite = Resources.Load<Sprite>("Image/" + name);
+
+        //用意していた変数に引数で受け取った情報を入れこむ
+        kinType = type;
+
+        //Tagをかえる
+        myTag = tag;
+        gameObject.tag = myTag;
+
+        //Tagに合わせて敵となるTagを設定する
+        if (gameObject.tag == "Enemy")
+        {
+            enemyTag = "Rival";
+        }
+        else
+        {
+            enemyTag = "Enemy";
+        }
+
+        //移動速度を生成位置(上下)に合わせてランダムに設定
+        if (isMoveDir)
+        {
+            //上で生成なら下方向に移動
+            speed = Random.Range(2.5f, 5);
+
+        }
+        else
+        {
+            //下で生成なら上方向に移動
+            speed = Random.Range(2.5f, 5);
+        }
+
+        state = State.SET_UP;
+
+    }
+
+
+    void Update()
+    {
+        //StateがSTOPなら、キンの動きを止めておく
+        if (state == State.STOP)
+        {
+            return;
+        }
+
+        //StateがSET_UPなら、キンの動きはじめる方向を決める
+        if (state == State.SET_UP && !isSetUp)
+        {
+            //一回だけ入るようにフラグで制御する
+            //Enumのみの場合、処理のタイミングによっては複数回入る可能性があるためフラグも作る
+            isSetUp = true;
+
+            //動き始める方向を設定
+            SetDirection();
+
+            //キンの移動を開始させる
+            state = State.MOVE;
+        }
+
+        //StateがMOVEなら、キンを移動させる
+        if (state == State.MOVE)
+        {
+            //移動
+            Move();
+        }
+    }
+
+
+    /// <summary>
+    /// キンの動き始める方向を設定
+    /// </summary>
+    private void SetDirection()
+    {
+        //移動先を360度のいずれかの方向をランダムで設定
+        direction = new Vector2(Random.Range(-2.5f, 2.5f), 1).normalized;
+    }
+
+
+
+    /// <summary>
+    ///キンの移動 
+    /// </summary>
+    private void Move()
+    {
+        //設定した方向に移動速度分ずつ移動させる
+        rb.velocity = speed * direction * transform.localScale.x;
+    }
+
+    /// <summary>
+    /// 当たり判定
+    /// </summary>
+    /// <param name="col"></param>
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Wall" || col.gameObject.tag == myTag)
+        {
+            //シャーレの壁か味方にぶつかった時に跳ね返る
+            SetDirection();
+        }
+
+        if (col.gameObject.tag == enemyTag)
+        {
+            //接触した相手が味方ではないなら破壊
+            Destroy(gameObject);
+        }
+    }
+}
