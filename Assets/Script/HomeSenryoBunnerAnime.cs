50e1e8d99764ceb6a77bbc57d1ca384fc71c3774
南志於 <au341998@gmail.com>
2020-02-11 14:23:01 +0900

    Homeシーンのバナーをアニメーションさせる

---
+++ Assets/Script/HomeSenryoBunnerAnime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HomeSenryoBunnerAnime : MonoBehaviour
+{
+    public Image bunner;
+
+    void Start()
+    {
+		//bunner.transform.DOShakePosition(1f, 50f, 3, 0, false, true).SetLoops(-1, LoopType.Yoyo);
+		//bunner.transform.DOPunchPosition(Vector3.one, 0.3f, 40, 5f, false).SetLoops(-1, LoopType.Yoyo);
+
+		bunner.transform.DOMoveY(0.01f, 0.5f).SetLoops(-1, LoopType.Yoyo);
+
+	}
+
+    
+}
