7914f604707571eb7579af99c55f77e0d16b641e
南志於 <au341998@gmail.com>
2020-02-15 14:50:46 +0900

    モデルをふわふわさせるアニメスクリプト入れた

---
+++ Assets/Script/Kin_Model/KinAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class KinAnimation : MonoBehaviour
+{
+    int frameCnt = 0; //フレームカウント
+    [Header("振り幅。ここを変えると上下の揺れ幅が変わる")]
+    public float amplitude = 0.001f;
+
+
+    //FixedUpdateはフレームレートの端末差を無くしてくれる。1フレームに一回の保証付
+    void FixedUpdate()
+    {
+        FuwaFuwa(); 
+    }
+
+
+    /// <summary>
+    /// ふわふわ動かす
+    /// </summary>
+    void FuwaFuwa()
+    {
+        frameCnt += 1;
+
+        if (10000 <= frameCnt)
+        {
+            frameCnt = 0;
+        }
+
+        if (0 == frameCnt % 2)
+        {
+            //上下に振動させる（ふわふわ表現）
+            float posYSin = Mathf.Sin(2.0f * Mathf.PI * (float)(frameCnt % 200) / (200.0f - 1.0f));
+            transform.DOMove(new Vector3(0, amplitude * posYSin, 0), 0.0f);
+
+        }
+    }
+
+}
